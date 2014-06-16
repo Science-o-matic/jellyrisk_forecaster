a57ede055206944161af4fbac17521f498afc860
dukebody <dukebody@gmail.com>
2014-06-16 17:34:53 +0200

    Modularize oneshot script.

--- src/oneshot.py
+++ src/oneshot.py
@@ -13,6 +13,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 API_KEY = os.environ.get("CARTODB_API_KEY")
 DOMAIN = os.environ.get("CARTODB_DOMAIN")
 
+TEMPFOLDER = os.path.join(BASE_DIR, 'tmp')
+
 if not API_KEY or not DOMAIN:
     raise EnvironmentError('Please set both CARTODB_API_KEY and CARTODB_DOMAIN environment variables.')
 
@@ -20,44 +22,64 @@ def quote(value):
     return "'%s'" % value
 
 
-# create temporal folder for the R script output files
-tempFolder = os.path.join(BASE_DIR, 'tmp')
-if not os.path.exists(tempFolder):
-    os.makedirs(tempFolder)
+
+### 0. create temporal folder for the R script output files
+
+def initialize():
+    if not os.path.exists(TEMPFOLDER):
+        os.makedirs(TEMPFOLDER)
 
 
 ### 1. Call R script
 
-with open(os.path.join(BASE_DIR, 'R/GuloGulo_calibrate_predict.R'), 'r') as inputfile:
-    call(["R", "--no-save"], stdin=inputfile)
+def calibrate_predict():
+    with open(os.path.join(BASE_DIR, 'R/GuloGulo_calibrate_predict.R'), 'r') as inputfile:
+        call(["R", "--no-save"], stdin=inputfile)
 
 
 ### 2. Open resulting CSV file and construct query
 
-values = []
+def construct_query():
+    values = []
 
-with open(os.path.join(tempFolder, 'GuloGuloEF.csv'), 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        value = ','.join([row['lon'], row['lat'], row['prob']])
-        values.append('(' + value + ')')
+    with open(os.path.join(TEMPFOLDER, 'GuloGuloEF.csv'), 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            value = ','.join([row['lon'], row['lat'], row['prob']])
+            values.append('(' + value + ')')
 
-query = """
-    INSERT INTO guloguloout (lon, lat, prob) 
-    VALUES %s;
-    """ % ', '.join(values)
+    query = """
+        INSERT INTO guloguloout (lon, lat, prob) 
+        VALUES %s;
+        """ % ', '.join(values)
+
+    return query
 
 
 ### 3. Connect to CartoDB and insert data
 
-cl = CartoDBAPIKey(API_KEY, DOMAIN)
+def insert_data(query):
+    cl = CartoDBAPIKey(API_KEY, DOMAIN)
 
-try:
-   print cl.sql(query)
-except CartoDBException as e:
-   print ("some error ocurred", e)
+    try:
+       print cl.sql(query)
+    except CartoDBException as e:
+       print ("some error ocurred", e)
+       raise
 
 
 ### -1: Clean up
 
-shutil.rmtree(tempFolder)
+def cleanup():
+    shutil.rmtree(TEMPFOLDER)
+
+
+def main():
+    initialize()
+    calibrate_predict()
+    query = construct_query()
+    insert_data(query)
+    cleanup()
+
+if __name__ == "__main__":
+    main()
