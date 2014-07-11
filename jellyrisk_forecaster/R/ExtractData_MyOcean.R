fe09255b09455b11c8d3f1bed8b3095ee951a743
dukebody <dukebody@gmail.com>
2014-07-11 13:02:16 +0200

    Impute raster layers using KNN.

--- jellyrisk_forecaster/R/ExtractData_MyOcean.R
+++ jellyrisk_forecaster/R/ExtractData_MyOcean.R
@@ -1,6 +1,27 @@
 library(ncdf4)
 library(raster)
+library(FNN)
 
+imputeKNN <- function(raster_layer) {
+    values <- getValues(raster_layer)
+    xy <- xyFromCell(raster_layer, 1:ncell(raster_layer))
+
+    missing <- is.na(values)
+    present <- !missing
+
+    missing_values <- values[missing]
+    present_values <- values[present]
+
+    neighs <- get.knnx(xy[present,], xy[missing,], k=1)
+
+    new_values <- values
+    new_values[missing] <- present_values[neighs$nn.index]
+
+    new_raster_layer <- raster_layer
+    new_raster_layer[] <- new_values
+
+    return(new_raster_layer)
+}
 
 # load the data in raster layers
 
@@ -10,28 +31,22 @@ rasterNitrate <- raster('MyOcean/myov04-med-ogs-bio-an-fc.nc', varname='nit')
 rasterChlorofile <- raster('MyOcean/myov04-med-ogs-bio-an-fc.nc', varname='chl')
 rasterPhosphate <- raster('MyOcean/myov04-med-ogs-bio-an-fc.nc', varname='pho')
 
-xmin <- 0
-xmax <- 5
-ymin <- 39
-ymax <- 44
-e <- extent(xmin, xmax, ymin, ymax)
-
-rasterSalinity.cropped <- crop(rasterSalinity, e)
-rasterTemperature.cropped <- crop(rasterTemperature, e)
-rasterNitrate.cropped <- crop(rasterNitrate, e)
-rasterChlorofile.cropped <- crop(rasterChlorofile, e)
-rasterPhosphate.cropped <- crop(rasterPhosphate, e)
+rasterSalinity.imputed <- imputeKNN(rasterSalinity)
+rasterTemperature.imputed <- imputeKNN(rasterTemperature)
+rasterNitrate.imputed <- imputeKNN(rasterNitrate)
+rasterChlorofile.imputed <- imputeKNN(rasterChlorofile)
+rasterPhosphate.imputed <- imputeKNN(rasterPhosphate)
 
 # read points where we want to interpolate the data (beaches)
 envBeaches <- read.table('Geo/beaches.txt', header=T)
 
 xy <- SpatialPoints(envBeaches[,c('lon', 'lat')])
 
-salinity.points <- extract(x=rasterSalinity.cropped, y=xy, method="bilinear")
-temperature.points <- extract(x=rasterTemperature.cropped, y=xy, method="bilinear")
-nitrate.points <- extract(x=rasterNitrate.cropped, y=xy, method="bilinear")
-chlorofile.points <- extract(x=rasterChlorofile.cropped, y=xy, method="bilinear")
-phosphate.points <- extract(x=rasterPhosphate.cropped, y=xy, method="bilinear")
+salinity.points <- extract(x=rasterSalinity.imputed, y=xy, method="bilinear")
+temperature.points <- extract(x=rasterTemperature.imputed, y=xy, method="bilinear")
+nitrate.points <- extract(x=rasterNitrate.imputed, y=xy, method="bilinear")
+chlorofile.points <- extract(x=rasterChlorofile.imputed, y=xy, method="bilinear")
+phosphate.points <- extract(x=rasterPhosphate.imputed, y=xy, method="bilinear")
 
 
 data <- data.frame(
